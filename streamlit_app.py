6dd4f37feb0639ba8c2afeb88644ae540e59c469
Overhoef <96155505+Overhoef@users.noreply.github.com>
2024-09-26 02:17:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,7 @@ from stocknews import StockNews
 #een klein nieuws tabje in de sidebar
 st.sidebar.header('Nieuws')
 sn = StockNews(ticker, save_news=False)
-df_news = sn.read_rss('AAPL')
+df_news = sn.read_rss()
 st.sidebar.write(df_news)
 
 pricing_data, fundamental_data, tech_indicator, signals = st.tabs(
